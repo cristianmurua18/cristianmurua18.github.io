3728486880f093a9cf660e669582769ab5d215ec
cristianmurua.18 <cristianmurua.18@gmail.com>
2023-03-24 21:50:08 -0300

    cambios3

--- javascript.js
+++ javascript.js
@@ -8,40 +8,42 @@ const numero = document.getElementById("telefono")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("errores")
 
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("form").addEventListener('submit', validarFormulario); 
+});
+
 // Creo una funcion para validar el formulario 
 
-function validacion(form) {
-    e.preventDefault()
-    let errores = ""
-    let entrar = false
-    let validaremail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+function validarFormulario(evento) {
+evento.preventDefault();
+let errores = ""
+let entrar = false
+let validaremail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-// Hace las validaciones necesarias para cumplir con los datos minimos necesarios para poder
-// enviar el formulario
+if(nombre.value.length <6){
+    errores += 'El nombre no es valido, debe tener mas de 6 caracteres <br>'
+    entrar = true
+}
 
-    if(form[0].value.length <6){
-       errores += 'El nombre no es valido, debe tener mas de 6 caracteres <br>'
-       entrar = true
-    }
+if(telefono.value.length<6){
+    errores += 'El telefono es muy corto, debe tener mas de 6 caracteres <br>'
+    entrar = true
+}
 
-    if(telefono.value.length<6){
-        errores += 'El telefono es muy corto, debe tener mas de 6 caracteres <br>'
-       entrar = true
-    }
+if(!validaremail.test(email.value)){
+    errores += 'El email no es valido, respeta el formato <br>'
+    entrar = true
+} 
 
-    if(!validaremail.test(email.value)){
-        errores += 'El email no es valido, respeta el formato <br>'
-        entrar = true
-    } 
-
-    if(entrar){
-        parrafo.innerHTML = errores
-    } else {
-        nombre.value = ""
-        email.value = ""
-        telefono.value = ""
-        consulta.value = ""
-        parrafo.innerHTML = "Formulario Enviado" 
-    }
+if(entrar){
+    parrafo.innerHTML = errores
+} else {
+    nombre.value = ""
+    email.value = ""
+    telefono.value = ""
+    consulta.value = ""
+    parrafo.innerHTML = "Formulario Enviado" 
+    this.submit();
+}
 }
 
